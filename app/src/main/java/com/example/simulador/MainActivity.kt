3439373ffd649f748a816b27c036891a123a5076
20232TIIMI0306 <lorrancalazans@gmail.com>
2025-09-18 17:52:39 -0300

    versao final

--- app/src/main/java/com/example/simulador/MainActivity.kt
+++ app/src/main/java/com/example/simulador/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.simulador
 import android.app.Activity
 import android.os.Bundle
 import android.renderscript.ScriptGroup
+import android.util.Log
 import android.view.inputmethod.InputBinding
 import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
@@ -19,21 +20,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    // teste 2
         val items = listOf("masculino", "feminino")
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        val adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item, items
+        )
 
         binding.spinner.adapter = adapter
 
         binding.ButtonCalcular.setOnClickListener {
-            val SexoSelecionado = binding.spinner.selectedItem as String
-            var idade = binding.TextIdade.text.toString().toLong()
-            var result: long
-            if (idade!=null) {
-                if (SexoSelecionado.trim().lowercase()="masculino") {
-                    result = 65 - idade
+            var SexoSelecionado = binding.spinner.selectedItem.toString()
+            var idadeString = binding.TextIdade.text.toString()
+            if (idadeString!="") {
+                var idade = idadeString.toLong()
+                var resultado: Long
+                var resultadoString: String
+                if (SexoSelecionado.trim().lowercase() == "masculino") {
+                    resultado = 65 - idade
+                } else {
+                    resultado = 62 - idade
                 }
+
+                if (resultado > 0) {
+
+                    if (resultado == 1L) {
+                        resultadoString = "falta 1 ano para você se aposentar"
+                    } else {
+                        resultadoString =
+                            "faltam " + resultado.toString() + " anos para você se aposentar"
+                    }
+                    binding.TextViewResultado.text = resultadoString
+                } else {
+                    binding.TextViewResultado.text = "Você já pode se aposentar"
+                }
+
             } else {
                 binding.TextViewResultado.text="informe uma idade"
             }
